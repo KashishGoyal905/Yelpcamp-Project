0aa8f21490664c8816b5f76c9a33ab9f2bd4cf0a
KashishGoyal905 <kashishgoyal961@gmail.com>
2021-07-17 10:29:55 +0530

    decribed untill comments

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
-const Schema  = mongoose.Schema;
+//making our first mongoose model
 
+//requiring mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//making schema details for products
 const CampgroundSchema = new Schema({
-    title:String,
-    price:String,
-    description:String,
-    location:String
+    title: String,
+    price: String,
+    description: String,
+    location: String
 });
 
-module.exports = mongoose.model('Campground',CampgroundSchema);
+//exporting mongoose schema so that we can use this in index.js
+module.exports = mongoose.model('Campground', CampgroundSchema);
 
